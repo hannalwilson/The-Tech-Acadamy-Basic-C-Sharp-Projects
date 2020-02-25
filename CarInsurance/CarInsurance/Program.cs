c40de83ca499649cf624abfadc583e926bd7b033
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-25 11:14:06 -0800

    Assignment page 101
    Program that uses boolean logic to determine if user is qualified for car insurance based on users input for age, dui, and number of speeding tickets

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string custAge = Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string hadDUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string numberTickets = Console.ReadLine();
+
+            int age = Convert.ToInt32(custAge);
+            bool DUI = Convert.ToBoolean(hadDUI);
+            int tickets = Convert.ToInt32(numberTickets);
+
+            bool qualifies = age > 15 && DUI == false && tickets <= 3;
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualifies);
+
+            Console.ReadLine();
+        }
+    }
+}
