9f996888cfd1b578d997e3090a49099ceec7678b
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-24 16:42:18 -0800

    Adding Variables and Page 72 assignment
    Learning variables and data types project added
    
    Assignment to create a daily report for Tech Academy students in the console added

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string page = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(page);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine();
+            bool needHelp = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string posExp = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study?");
+            string study = Console.ReadLine();
+            int studyHours = Convert.ToInt32(study);
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
