803a3f6482ea34319c6c07abe5e6ec178005db05
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-25 11:54:15 -0800

    Assignment page 123
    Program that gets a packages weight and dimensions and, if the package if small enough to ship, determines the cost of shipping

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length;");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int dim = width + height + length;
+            int quote = (weight * width * height * length) / 100;
+
+            if (dim > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+            }
+
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
