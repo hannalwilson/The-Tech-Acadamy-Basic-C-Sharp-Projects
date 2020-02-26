2ed40ca297229b42a1cc6326c55d8656fbedaa29
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-25 16:51:11 -0800

    Intro to Array and Lists
    Practicing with the basics of lists and arrays

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+            //numArray2[5] = 650;
+
+            //Console.WriteLine(numArray2[5]);
+            //Console.ReadLine();
+
+            List<string> intList = new List<string>();
+            intList.Add("Hello");
+            intList.Add("Hanna");
+            intList.Remove("Hanna");
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
