4c10a73ca7f39341b96055122bd5ad366f2668ce
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-26 13:31:34 -0800

    Assignment page 147
    Practice with exception handling

---
+++ ListExceptionHandling/ListExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> numbers = new List<int> { 40, 22, 36, 4, 8, 50 };
+
+                Console.WriteLine("Pick a number, any number!");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i] = numbers[i] / divisor;
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("The program has emerged from the try/catch block.");
+            Console.ReadLine();
+        }
+    }
+}
