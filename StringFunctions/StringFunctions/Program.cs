a106edb8a2177fbf76dc2e9e1f3b6fdbf3d20f8a
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-25 16:35:35 -0800

    Assignment page 131
    Practice using string functions

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Hanna";
+            //string quote = "The man said, \"Hello\", Hanna. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\Hanna";
+
+            //bool trueOrFalse = name.Contains("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(quote);
+            //Console.WriteLine(fileName);
+            //Console.WriteLine(trueOrFalse);
+            //Console.WriteLine(length);
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Hanna");
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringPractice/StringPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello and welcome to my string practice console! ";
+            string enjoy = ", I hope you enjoy what you see!";
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(greeting + name + enjoy);
+            Console.ReadLine();
+
+            string ready = "get ready for the next part!!!!!";
+            ready = ready.ToUpper();
+
+            Console.WriteLine(ready);
+            Console.ReadLine();
+
+            StringBuilder paragraph = new StringBuilder("Guess what!? ");
+
+            paragraph.Append("I'm practicing different things you can do with strings in Visual Studio. ");
+            paragraph.Append("I've learned how to concatenate strings. ");
+            paragraph.Append("I've also learned how to convert a string to upper or lower case. ");
+            paragraph.Append("Now, I'm learning stringbuilder. ");
+            paragraph.Append("It's pretty neat! ");
+            paragraph.Append("You probably can't tell but I am building this paragraph one sentence at a time. ");
+            paragraph.Append("I can do that by utilizing stringbuilder and the append function. ");
+            paragraph.Append("At least, I hope that is how I'm supposed to do it...");
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+
+        }
+    }
+}
