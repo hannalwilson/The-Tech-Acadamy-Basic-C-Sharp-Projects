c6377e7b55c0e2c3bfd68f1ff1f691febd059ab6
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-27 15:22:40 -0800

    Assignment page 161
    Created a void method with two integers as parameters

---
+++ VoidMethod/VoidMethod/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Math
+    {
+        public void MathOperation(int num1, int num2)
+        {
+            int result = num1 + 5;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            int numOne = 450;
+            int numTwo = 10;
+            math.MathOperation(numOne, numTwo);
+            Console.ReadLine();
+
+        }
+    }
+}
