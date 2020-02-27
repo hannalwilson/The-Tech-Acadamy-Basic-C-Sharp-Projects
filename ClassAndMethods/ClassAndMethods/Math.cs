f2fb9048b232609c094215d083d0b39a6bb015d5
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-27 09:25:12 -0800

    Assignment page 156
    Practice creating classes and methods and calling on the methods

---
+++ ClassAndMethods/ClassAndMethods/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    public class Math
+    {
+        public static void Addition(int numAdd, out int resultAdd)
+        {
+            resultAdd = numAdd + 10;
+        }
+
+        public static void Subtraction(int numSub, out int resultSub)
+        {
+            resultSub = 100 - numSub;
+        }
+
+        public static void Multiply(int numMult, out int resultMult)
+        {
+            resultMult = numMult * 3;
+        }
+    }
+}
---
+++ ClassAndMethods/ClassAndMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+            Math.Addition(num, out result);
+            Console.WriteLine(num + " + 10 = " + result);
+            Console.ReadLine();
+
+            Math.Subtraction(num, out result);
+            Console.WriteLine("100 - " + num + " = " + result);
+            Console.ReadLine();
+
+            Math.Multiply(num, out result);
+            Console.WriteLine(num + " x 3 = " + result);
+            Console.ReadLine();
+        }
+    }
+}
