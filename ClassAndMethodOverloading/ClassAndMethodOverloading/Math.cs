5ed5fd0d08b1f3c39bc48a57a0a9fd8cd8137e35
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-27 10:03:48 -0800

    Assignment page 159
    Practice utilizing method overloading with basic math operations

---
+++ ClassAndMethodOverloading/ClassAndMethodOverloading/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodOverloading
+{
+    public class Math
+    {
+        public int MathOperation(int number)
+        {
+            int result = number + 50;
+            return result;
+        }
+
+        public int MathOperation(decimal number)
+        {
+            int result = Convert.ToInt32(number * 5);
+            return result;
+        }
+
+        public int MathOperation(string number)
+        {
+            int result = Convert.ToInt32(number) - 1;
+            return result;
+        }
+    }
+}
---
+++ ClassAndMethodOverloading/ClassAndMethodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Math math = new Math();
+            int result = math.MathOperation(number);
+            Console.WriteLine(number + " + 50 = " + result);
+
+            Console.WriteLine("Pick another number, with a decimal:");
+            decimal numberDec = Convert.ToDecimal(Console.ReadLine());
+            int resultDec = math.MathOperation(numberDec);
+            Console.WriteLine(numberDec + " x 5 = " + resultDec);
+
+            Console.WriteLine("One last time, pick a number:");
+            string numberStr = Console.ReadLine();
+            int resultStr = math.MathOperation(numberStr);
+            Console.WriteLine(numberStr + " - 1 = " + resultStr);
+
+            Console.ReadLine();
+        }
+    }
+}
