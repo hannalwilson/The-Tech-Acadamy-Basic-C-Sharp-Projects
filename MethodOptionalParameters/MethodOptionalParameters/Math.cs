2d84c8da91386c608c7b47ad2ba5a12111c978ed
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-27 14:12:20 -0800

    Assignment page 160
    A method with two parameters and one of them already has a value

---
+++ MethodOptionalParameters/MethodOptionalParameters/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParameters
+{
+    public class Math
+    {
+        public int MathOperation(int numOne, int numTwo = 2)
+        {
+            int result = 10 * numOne / numTwo;
+            return result;
+        }
+    }
+}
---
+++ MethodOptionalParameters/MethodOptionalParameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Pick a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you want to, pick another number, otherwise just press enter again.");
+            string optionalNumber = Console.ReadLine();
+
+
+            if (optionalNumber == "")
+            {
+                int answer = math.MathOperation(number);
+                Console.WriteLine("10 x " + number + " / 2 = " + answer);
+                Console.ReadLine();
+            }
+            else
+            {
+                int number2 = Convert.ToInt32(optionalNumber);
+                int answer2 = math.MathOperation(number, number2);
+                Console.WriteLine("10 x " + number + " / " + number2 + " = " + answer2);
+                Console.ReadLine();
+            }
+         
+            
+        }
+    }
+}
