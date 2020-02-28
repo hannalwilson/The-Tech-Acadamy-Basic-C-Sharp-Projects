453a40bd8c47d2c3e8e5e735162364a286ebb332
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-02-27 17:50:10 -0800

    Assignment page 164
    Create a void method with an output, overload a method

---
+++ VoidOverloadMethods/VoidOverloadMethods/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidOverloadMethods
+{
+    public class Math
+    {
+        public static void Divide(int num, out int result)
+        {
+            result = num / 2;
+
+        }
+
+        public static void Output(out int numOne, out int numTwo)
+        {
+            numOne = 1;
+            numTwo = 2;
+        }
+
+        public static void Output(out int numOne, out int numTwo, out int numThree)
+        {
+            numOne = 100;
+            numTwo = 200;
+            numThree = 300;
+        }
+    }
+}
---
+++ VoidOverloadMethods/VoidOverloadMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidOverloadMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Enter a number, or else.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+            Math.Divide(num, out result);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
