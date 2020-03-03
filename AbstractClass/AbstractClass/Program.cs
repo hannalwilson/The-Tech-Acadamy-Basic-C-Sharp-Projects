a757d587ae95b15b809605b8713a9db795ceb79d
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-03 09:28:25 -0800

    Assignment page 176
    Practice using abstract class and method, and inheriting from them.

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
