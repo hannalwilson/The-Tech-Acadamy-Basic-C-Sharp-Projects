b238f7aab229ec9c39d40e01082eab58814de2b8
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-05 11:41:27 -0800

    Assignment page 187
    Using generics to create lists

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T>
+    {
+        public List<T> things { get; set; }
+
+    }
+}
