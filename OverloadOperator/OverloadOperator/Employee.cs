bbc721cf3e76680662d9b383077bffc685c70b04
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-05 10:56:26 -0800

    Assignment page 184
    Practice overloading operators, specifically the == operator.

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ID { get; set; }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            bool isEqual = (emp1.ID == emp2.ID);
+            return isEqual;
+        }
+
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            bool notEqual = (emp1.ID != emp2.ID);
+            return notEqual;
+        }
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee() { ID = 1 };
+            Employee emp2 = new Employee() { ID = 2 };
+
+            bool isEqual = emp1.ID == emp2.ID;
+
+            if (isEqual == true)
+            {
+                Console.WriteLine("The employees are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The employees are not equal.");
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+    
+}
