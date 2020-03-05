ee9a7fd4f7fb9f981e4fc13f7b0ccc8594c5bd5f
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-05 13:33:17 -0800

    Assignment page 192
    Created a enum and checked user input to make sure it was a value in the enum

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter what day of the week it is (hint: capitalize the first letter)");
+            try
+            {
+                DaysOfTheWeek answer = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+
+    }
+    public enum DaysOfTheWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
