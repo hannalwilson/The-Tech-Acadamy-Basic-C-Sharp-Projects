fa2726b6d36edd76ce119fe0d9151c3b031f30b9
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-10 14:42:00 -0700

    Assignment page 218
    Practice logging user input into a text file, then reading the contents of the file and printing it to the console.

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Desktop\Logs\log.txt", true))
+            {
+                file.WriteLine(answer);
+            }
+
+            using (StreamReader readFile = new StreamReader(@"C:\Users\Student\Desktop\Logs\log.txt"))
+            {
+                string line = readFile.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = readFile.ReadLine();
+                }
+                readFile.Close();
+            }
+            Console.ReadLine();
+              
+        }
+    }
+}
