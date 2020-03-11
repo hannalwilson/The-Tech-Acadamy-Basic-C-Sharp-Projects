238e03cec52318621e5a275846f2cd6b407b5147
hannalwilson <57196027+hannalwilson@users.noreply.github.com>
2020-03-11 13:44:16 -0700

    Assignment page 231
    Using const variables and chain constructors

---
+++ ConstVariable/ConstVariable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string title = "Queen";
+            var name = "Hanna";
+
+            Royalty royalty = new Royalty(title);
+            Console.WriteLine("{0} {1} says \"{2}\"", royalty.Title, name, royalty.Phrase);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstVariable/ConstVariable/Royalty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariable
+{
+    class Royalty
+    {
+        public Royalty(string title) : this(title, "Bow down peasants!")
+        {
+
+        }
+        public Royalty(string title, string phrase)
+        {
+            Title = title;
+            Phrase = phrase;
+
+        }
+
+        public string Title { get; set; }
+        public string Phrase { get; set; }
+    }
+}
